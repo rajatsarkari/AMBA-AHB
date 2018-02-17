c64263490a660e941467f988dcdfbbd859214dc1
Rajat Sarkari <rajatsarkari777@gmail.com>
2018-02-18 00:01:52 +0530

    Updated

--- AMBA_AHB_SLAVE.v
+++ AMBA_AHB_SLAVE.v
@@ -1,362 +1,196 @@
-//Global Parameters
-`define IDLE  		3'b000
-`define ACTIVE		3'b001
-`define AGAIN		3'b010
-`define LITTLE		3'b011
-
-	
-`define NON_SEQ		2'b00
-`define SEQ		2'b01
+//Global Parameters	
+`define IDLE		2'b00
 `define BUSY		2'b01
-`define IDLE_TRANS	2'b11
+`define NON_SEQ		2'b10
+`define SEQ		2'b11
 
 
 `define OKAY  		2'b00
 `define ERROR		2'b01
 `define RETRY		2'b10
 `define SPLIT		2'b11
-
-module amba_ahb_slave(HREADY, HRESP, HRDATA, HSPLITx, HSELx, HADDR, HWRITE, HTRANS, HSIZE, HBURST, HWDATA, HRESETn, HCLK, HMASTER, HMASTLOCK);
-
+//Top Level Module AMBA AHB SLAVE
+module AMBA_AHB_SLAVE(HREADY, HRESP, HRDATA, HSPLITx, HSELx, HADDR, HWRITE, HTRANS, HSIZE, HBURST, HWDATA, HRESETn, HCLK, HMASTER, HMASTLOCK);
 	//Input Output Declarations
-	output 		HREADY;
-	output	[1:0]	HTRANS;
-	output	[1:0]	HRESP;
-	output	[31:0]	HRDATA;
-	output  [15:0]	HSPLITx;
-
-	input		HMASTLOCK, HSELx, HWRITE, HRESETn, HCLK;
-	input	[31:0]	HADDR;
-	input	[31:0]	HWDATA;
-	input	[2:0]	HBURST;
-	input	[2:0]	HSIZE;
-	input	[3:0]	HMASTER;	
+	output 				HREADY;
+	output	[1:0]			HRESP;
+	output	[31:0]			HRDATA;
+	output  [15:0]			HSPLITx;
+	
+	input				HMASTLOCK, HSELx, HWRITE, HRESETn, HCLK;
+	input	[1:0]			HTRANS;
+	input	[31:0]			HADDR;
+	input	[31:0]			HWDATA;
+	input	[2:0]			HBURST;
+	input	[2:0]			HSIZE;
+	input	[3:0]			HMASTER;	
 	//Reg Declarations
-	integer		count;
-
-	reg		HREADY;
-	reg	[1:0]	HTRANS;
-	reg	[1:0]	HRESP;
-	reg	[31:0]	HRDATA;
-	reg	[15:0]	HSPLITx;
-
-	reg	[31:0]	HADDR_temp;
-	reg	[3:0]	SPLIT_RESP;
-	reg	[2:0]	ps, ns;
-	reg	[31:0]	memory_slave[0:31];
-
-	//State Transitions
-	always@(posedge HCLK) 			ps	<= ns;
-	//Operation Starts
-	always@(ps or ns or HRESETn or HSELx or HWDATA or HWRITE)
-		case(ps)
-		`IDLE:		if(!HRESETn && HSELx == 0)
-					ns		= `IDLE;
-				else begin
-					HREADY		= 1;
-					HADDR_temp	= HADDR;
-					ns		= `ACTIVE;
-					HTRANS		= `NON_SEQ;
+	integer				COUNT_R, COUNT_W;
+	
+	reg				HREADY, WRAP;
+	reg	[1:0]			HRESP;
+	reg	[31:0]			HRDATA;
+	reg	[15:0]			HSPLITx;
+	//LOCAL VARIABLES
+	reg	[3:0]			SPLIT_RESP;
+	reg	[7:0]			mem[0:1024];
+	//STAGE S1 PIPE REGISTERS
+	reg	[31:0]			HADDR_S1;
+	reg 	[31:0]			BASE_HADDR;
+	reg	[1:0]			HTRANS_S1;
+	reg	[31:0]			NUMBER_BYTES;
+	reg	[31:0]			BURSTLEN, DT_SIZE, LOWWRAP, UPWRAP, UBL, LBL;
+	reg	[31:0]			BURST_LEN_S1;
+	reg 	[1:0]			HRESP_S1;
+	//PARAMETERS
+	localparam	SINGLE		= 3'D0,
+			INCR		= 3'D1,
+			WRAP4		= 3'D2,
+			INCR4		= 3'D3,
+			WRAP8		= 3'D4,
+			INCR8		= 3'D5,
+			WRAP16		= 3'D6,
+			INCR16		= 3'D7;
+	//STAGE S1 ----------------------------------ADDRESS AND CONTROL PHASE--------------------------------------------------------------------
+	always@(posedge HCLK or negedge HRESETn)
+		//Reset
+		if(!HRESETn && HSELx) fork
+			HREADY		<= 1;
+			HRESP		<= `OKAY;
+			HRDATA		<= 0;
+			HSPLITx 	<= 0;
+		join
+		//Set
+		else fork
+			if(HRESETn && HSELx) fork
+				//STAGE S1 PIPE registers 
+				HTRANS_S1  	<= HTRANS;
+				BASE_HADDR 	<= HADDR;
+				HADDR_S1 	<= HADDR;
+				HREADY 		<= 1;
+				HRESP	 	<= `OKAY;
+				if(HBURST == SINGLE) begin
+				BURSTLEN 	<= 1;
 				end
-		`ACTIVE:	fork//HSIZE NOT Considered for the sake of simplicity
-				if(HRESETn && HSELx && HWRITE && HREADY)
-					case(HBURST)
-					3'd0:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HREADY			= 1;
-						HRESP			= `OKAY;
-						HTRANS			= `NON_SEQ;
-						ns			= `IDLE;		
-					     end	
-					3'd1:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 1;
-						count			= count + 1;
-						if(count > 32)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns		= `ACTIVE;	
-					     end
-					3'd2:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 4)
-						begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-						else begin
-							ns 		= `ACTIVE;
-							if(HADDR_temp[7:0] > 8'd12)
-							begin
-							//Address Wrap
-							end
-					 	end
-					     end	
-					3'd3:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;		
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 4)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns 		= `ACTIVE;
-					     end
-					3'd4:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 8)
-						begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-						else begin
-							ns 		= `ACTIVE;
-							if(HADDR_temp[7:0] > 8'd12)
-							begin
-							//Address Wrap
-							end
-					 	end
-					     end	
-					3'd5:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;		
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 8)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns 		= `ACTIVE;
-					     end
-					3'd6:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 16)
-						begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-						else begin
-							ns 		= `ACTIVE;
-							if(HADDR_temp[7:0] > 8'd12)
-							begin
-							//Address Wrap
-							end
-					 	end
-					     end	
-					3'd7:begin
-						memory_slave[HADDR_temp]= HWDATA;
-						HTRANS			= `SEQ;		
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 16)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns 		= `ACTIVE;
-					     end					
-					default:begin
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-					endcase
-				if(HRESETn && HSELx && !HWRITE && HREADY)
-					case(HBURST)
-					3'd0:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HREADY			= 1;
-						HRESP			= `OKAY;
-						HTRANS			= `NON_SEQ;
-						ns			= `IDLE;		
-					     end	
-					3'd1:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 1;
-						count			= count + 1;
-						if(count > 32)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns		= `ACTIVE;	
-					     end
-					3'd2:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 4)
-						begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-						else begin
-							ns 		= `ACTIVE;
-							if(HADDR_temp[7:0] > 8'd12)
-							begin
-							//Address Wrap
-							end
-					 	end
-					     end	
-					3'd3:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;		
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 4)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns 		= `ACTIVE;
-					     end
-					3'd4:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 8)
-						begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-						else begin
-							ns 		= `ACTIVE;
-							if(HADDR_temp[7:0] > 8'd12)
-							begin
-							//Address Wrap
-							end
-					 	end
-					     end	
-					3'd5:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;		
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 8)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns 		= `ACTIVE;
-					     end
-					3'd6:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 16)
-						begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-						else begin
-							ns 		= `ACTIVE;
-							if(HADDR_temp[7:0] > 8'd12)
-							begin
-							//Address Wrap
-							end
-					 	end
-					     end	
-					3'd7:begin
-						HRDATA			= memory_slave[HADDR_temp];
-						HTRANS			= `SEQ;		
-						HADDR_temp		= HADDR_temp + 4;
-						count			= count + 1;
-						if(count > 16)
-							begin
-							count		= 0;
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-							end
-						else 	ns 		= `ACTIVE;
-					     end					
-					default:begin
-							ns		= `IDLE;
-							HREADY		= 1;	
-							HRESP		= `OKAY;
-							HTRANS		= `BUSY;
-						end
-					endcase
-				if(!HREADY)
+				if(HBURST == INCR) begin
+				BURSTLEN 	<= 4;		
+				end
+				if(HBURST == WRAP4) begin
+				BURSTLEN 	<= 4;
+				WRAP  		<= 1;		
+				end
+				if(HBURST == INCR4) begin
+				BURSTLEN 	<= 4;		
+				end
+				if(HBURST == WRAP8) begin
+				BURSTLEN 	<= 8;
+				WRAP  		<= 1;		
+				end
+				if(HBURST == INCR8) begin
+				BURSTLEN 	<= 8;		
+				end
+				if(HBURST == WRAP16) begin
+				BURSTLEN 	<= 16;
+				WRAP  		<= 1;		
+				end
+				if(HBURST == INCR16) begin
+				BURSTLEN 	<= 16;		
+				end
+				BURST_LEN_S1	<= BURSTLEN;
+				NUMBER_BYTES 	<= 2**HSIZE;
+				//DT_SIZE	<= NUMBER_BYTES * BURSTLEN;
+				//ALIGNED_ADDR	<= (HADDR/NUMBER_BYTES) * NUMBER_BYTES;
+				//ALIGNED	<= (HADDR == ALIGNED_ADDR);
+//				LOWWRAP		<= (HADDR/DT_SIZE) * DT_SIZE;
+//				UPWRAP		<= LOWWRAP + DT_SIZE;
+				LBL		<= HADDR - ((HADDR/4)*4);
+				UBL 		<= LBL + NUMBER_BYTES - 1;
+				//HMASTER CHECK
+			join
+			/*2-CYCLE RESPONSE CHECK FOR HRESP
+			if(HRESP == `SPLIT || HRESP == `RETRY || HRESP == `ERROR) begin
+				HRESP_S1 	<= HRESP;
+				HREADY 		<= 0;
+			end*/
+		join
+	//STAGE S2 -------------------------------------DATA PHASE--------------------------------------------------------------------------------
+	always@(posedge HCLK) begin
+		if(HSELx && HREADY && HRESETn) begin
+			DT_SIZE		<= NUMBER_BYTES * BURSTLEN;
+			LOWWRAP		<= (HADDR/DT_SIZE) * DT_SIZE;
+			UPWRAP		<= LOWWRAP + DT_SIZE;			
+		end
+		case(HTRANS_S1)
+			`IDLE		://NO DATA TRANSFER
 					begin
-					ns 	 	= `AGAIN;
-					HRESP		= `RETRY;
-					end
-				join
-		`AGAIN:		if(HREADY)
-					ns		= `ACTIVE;
-				else
-					ns		= `LITTLE;
-		`LITTLE:	begin
-					HRESP				= `SPLIT;
-					SPLIT_RESP			= HMASTER;
-					if(HMASTLOCK)
-						ns			= `ACTIVE;
-					else begin
-						HSPLITx			= SPLIT_RESP;
-						ns			= `IDLE;
+					HRESP <= `OKAY;
 					end		
-				end
-		endcase
-	//Operation Ends			
+			`BUSY		://WAIT BETWEEN BURSTS
+					begin
+					HRESP <= `OKAY;
+					end
+			`NON_SEQ	://FIRST TRANSFER OF THE BURST OR THE SINGLE TRANSFER
+					fork
+					//2-CYCLE RESPONSE CHECK FOR HRESP
+					if(HRESP_S1 == `SPLIT || HRESP_S1 == `RETRY || HRESP_S1 == `ERROR) begin
+							HREADY 		<= 0;
+					end
+					//WRITE CHANNEL
+					if(HWRITE && HREADY && HSELx && HRESP == `OKAY) fork
+						for(COUNT_W = LBL; COUNT_W <= UBL; COUNT_W = COUNT_W + 1) begin
+							mem[HADDR_S1] 	<= HWDATA[COUNT_W+:7];
+							HADDR_S1 	<= HADDR_S1 + 1;
+						end
+							HRESP 		<= `OKAY;
+							COUNT_W 	<= 0;
+					join
+					//READ CHANNEL
+					if(!HWRITE && HREADY && HSELx && HRESP == `OKAY) fork
+						for(COUNT_R = LBL; COUNT_R <= UBL; COUNT_R = COUNT_R + 1) begin
+							HRDATA[COUNT_R+:7] 	<= mem[HADDR_S1];
+							HADDR_S1 		<= HADDR_S1 + 1;
+						end
+							HRESP 		<= `OKAY;
+							COUNT_R		<= 0;
+					join
+					join
+			`SEQ		://BURST TRANSFER
+					fork
+					//2-CYCLE RESPONSE CHECK FOR HRESP
+					if(HRESP_S1 == `SPLIT || HRESP_S1 == `RETRY || HRESP_S1 == `ERROR) begin
+							HREADY 		<= 0;
+							HADDR_S1	<= BASE_HADDR;
+					end
+					//WRITE CHANNEL
+					if(HWRITE && HREADY && HSELx && HRESP == `OKAY) fork
+						if(BURST_LEN_S1 > 1) begin
+							for(COUNT_W = LBL; COUNT_W <= UBL; COUNT_W = COUNT_W + 1) begin
+								mem[HADDR_S1] 	<= HWDATA[COUNT_W+:7];
+								HADDR_S1	<= HADDR_S1 + 1;
+							end
+							if(HADDR_S1 >= UPWRAP) HADDR_S1 <= LOWWRAP;
+							BURST_LEN_S1 	<= BURST_LEN_S1 - 1;
+							HRESP 		<= `OKAY;
+						end
+					join
+					//READ CHANNEL
+					if(!HWRITE && HREADY && HSELx && HRESP == `OKAY) fork
+						if(BURST_LEN_S1 > 1) begin
+							for(COUNT_R = LBL; COUNT_R <= UBL; COUNT_R = COUNT_R + 1) begin
+								HRDATA[COUNT_R+:7] 	<= mem[HADDR_S1];
+								HADDR_S1 		<= HADDR_S1 + 1;
+							end
+							if(HADDR_S1 >= UPWRAP) HADDR_S1 <= LOWWRAP;						
+							BURST_LEN_S1 	<= BURST_LEN_S1 - 1;
+							HRESP 		<= `OKAY;
+						end
+					join
+					join
+			default:	begin
+					HRESP	<= `OKAY;
+					HREADY  <= 1;
+					end
+		endcase	
+	end
+	//--------------------------------------------------------------------------------------------------------------------------------------- 
 endmodule
